5de952184f3c075d932eafc914b239156f1a6032
Piyush Kumar Jha <105655779+Piyush5784@users.noreply.github.com>
2024-04-26 11:55:47 +0530

    links added

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -5,16 +5,19 @@ const Footer = () => {
   return (
     <>
       <div className="text-3xl text-gray-500 flex gap-4 dark:bg-black dark:text-white bg-white justify-center items-center p-10 pt-0">
-        <a href="">
+        <a href="https://twitter.com/Piyush5784" target="_blank">
           <FaTwitter></FaTwitter>
         </a>
-        <a href="">
+        <a
+          href="https://www.linkedin.com/in/piyush-jha-a29619239/"
+          target="_blank"
+        >
           <FaLinkedin />
         </a>
-        <a href="">
+        <a href="https://discord.com/" target="_blank">
           <FaDiscord />
         </a>
-        <a href="">
+        <a href="mailto:piyushjha5668@gmail.com">
           <IoMail />
         </a>
       </div>
